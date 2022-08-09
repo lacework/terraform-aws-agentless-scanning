f986ba95b1fed62caebb51499a3d0bb6eada426f
Alan Nix <alan.nix@lacework.net>
2022-08-08 23:33:45 -0400

    Added specific providers to avoid warnings

--- versions.tf
+++ versions.tf
@@ -2,6 +2,8 @@ terraform {
   required_version = ">= 0.15.0"
 
   required_providers {
+    aws    = "~> 4.0"
+    random = ">= 2.1"
     lacework = {
       source  = "lacework/lacework"
       version = "~> 0.23"
