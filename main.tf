a83f29df7b6151d6562814e74e83f349df112d6c
Teddy Reed <teddy.reed@lacework.net>
2022-11-08 23:08:39 -0500

    feat: Add default encryption and public access block to S3

--- main.tf
+++ main.tf
@@ -473,6 +473,27 @@ resource "aws_s3_bucket" "agentless_scan_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "agentless_scan_bucket_public_access_block" {
+  count  = var.global ? 1 : 0
+  bucket = aws_s3_bucket.agentless_scan_bucket[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "agentless_scan_bucket_encryption" {
+  count  = var.global ? 1 : 0
+  bucket = aws_s3_bucket.agentless_scan_bucket[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
   count  = var.global ? 1 : 0
   bucket = aws_s3_bucket.agentless_scan_bucket[0].id
