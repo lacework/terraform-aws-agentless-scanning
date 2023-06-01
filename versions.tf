11ad3d87d54236e7553ab011dc01e726344a6617
Jannes Heinrich <jannes.heinrich@be-bold.today>
2023-06-01 11:09:14 +0200

    chore: Updated provider constraint to allow aws provider 5.x This change allows not only the usage of aws provider version 4.x, but also the usage of 5.x.

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.15.0"
 
   required_providers {
-    aws    = "~> 4.0"
+    aws    = ">= 4.0.0, < 6.0.0"
     random = ">= 2.1"
     lacework = {
       source  = "lacework/lacework"
