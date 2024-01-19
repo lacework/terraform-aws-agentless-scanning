e1bfba480a06c9323f11dde4377eb130584f92f3
Darren Murray <darren.murray@lacework.net>
2024-01-19 13:36:05 +0000

    chore: add lacework_metric_module datasource
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- main.tf
+++ main.tf
@@ -56,6 +56,10 @@ locals {
       value = "ORCHESTRATE"
     },
   ]
+  
+  version_file   = "${abspath(path.module)}/VERSION"
+  module_name    = basename(abspath(path.module))
+  module_version = fileexists(local.version_file) ? file(local.version_file) : ""
 }
 
 data "aws_region" "current" {}
@@ -1114,4 +1118,9 @@ resource "aws_cloudwatch_event_target" "agentless_scan_event_target" {
 
 resource "null_resource" "check_organization_requires_global_input" {
   count = length(var.organization.monitored_accounts) > 0 ? (var.global ? 0 : "Error: When var.organization is used then var.global must also = true") : 0
+}
+
+data "lacework_metric_module" "lwmetrics" {
+  name    = local.module_name
+  version = local.module_version
 }
